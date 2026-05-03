3d562990170ecb4058744b8328c23eee944011c2
Gustavo Rodrigues Barbara Moreira <gustavorrbbmoreira@gmail.com>
2026-05-03 18:24:59 -0300

    criação da estrutura backend com spring boot

---
+++ back/SisHosp/src/main/java/com/grupo4/SisHosp/SisHospApplication.java
@@ -0,0 +1,13 @@
+package com.grupo4.SisHosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisHospApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisHospApplication.class, args);
+	}
+
+}
---
+++ back/SisHosp/src/test/java/com/grupo4/SisHosp/SisHospApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo4.SisHosp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisHospApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
