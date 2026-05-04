e86cae22bae72cfd6290f073aa3e50e9dfb80906
Anny Victorya Azevedo Oliveira <annyvictoryaaoliveira@gmail.com>
2026-05-04 20:18:01 -0300

    criação de Aluguel repository

---
+++ back/SisHosp/src/main/java/com/grupo4/SisHosp/repository/AluguelRepository.java
@@ -0,0 +1,6 @@
+package com.grupo4.SisHosp.repository;
+
+import com.grupo4.SisHosp.model.Aluguel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AluguelRepository extends JpaRepository<Aluguel, Long> {}
