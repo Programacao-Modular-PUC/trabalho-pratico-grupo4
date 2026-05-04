dc7d8373a0709920f98de9e214865c0224fc87da
Gustavo Rodrigues Barbara Moreira <gustavorrbbmoreira@gmail.com>
2026-05-04 20:43:49 -0300

    adição do quarto Repository

---
+++ back/SisHosp/src/main/java/com/grupo4/SisHosp/repository/QuartoRepository.java
@@ -0,0 +1,6 @@
+package com.grupo4.SisHosp.repository;
+
+import com.grupo4.SisHosp.model.Quarto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuartoRepository extends JpaRepository<Quarto, Long> {}
