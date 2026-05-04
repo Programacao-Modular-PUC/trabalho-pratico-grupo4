2352d0b9af0dae183d3b03545886e263e392e55b
Gustavo Rodrigues Barbara Moreira <gustavorrbbmoreira@gmail.com>
2026-05-04 20:07:39 -0300

    criação do quarto e residencia Repository

---
+++ back/SisHosp/src/main/java/com/grupo4/SisHosp/repository/ResidenciaRepository.java
@@ -0,0 +1,7 @@
+package com.grupo4.SisHosp.repository;
+
+import com.grupo4.SisHosp.model.Residencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResidenciaRepository extends JpaRepository<Residencia, Long> {}
+
